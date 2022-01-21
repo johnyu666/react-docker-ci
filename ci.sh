ea241b8831ea0dca168c50e15945dac88c1e1240
yuyinghan <yuyinghan@yeah.net>
2022-01-21 16:56:50 +0800

    dfd

--- ci.sh
+++ ci.sh
@@ -2,6 +2,7 @@
 npm run build
 cd build
 tar cvf ../docker-front/dist.tar .
+# 生成docker镜像，并推送
 cd ../docker
 docker build -t registry.cn-zhangjiakou.aliyuncs.com/johnyucn525/react-albums-httpd .
 docker push registry.cn-zhangjiakou.aliyuncs.com/johnyucn525/react-albums-httpd
